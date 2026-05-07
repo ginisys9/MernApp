03b2ea1c661affa57cb47a37a24fd24eb62d8b3c
Md Intekhab <ginisys9@gmail.com>
2026-05-07 23:55:30 +0530

    Implement bookmark feature

--- frontend/pages/Bookmark.jsx
+++ frontend/pages/Bookmark.jsx
@@ -9,6 +9,7 @@ const Bookmarks = () => {
       const user = JSON.parse(
         localStorage.getItem("user")
       );
+      
       if (!user || !user.bookmarks) {
         setLoading(false);
         return;
