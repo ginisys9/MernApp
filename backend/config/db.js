a43e5414660ff91ecfde582c97315c63143d188a
Md Intekhab <ginisys9@gmail.com>
2026-05-08 14:28:05 +0530

    Call scrapeAndSave after connecting to the database
    Invoke scrapeAndSave function after successful database connection.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose =require('mongoose');
+const { scrapeAndSave }  = require("../scraper/scraper")
+
 const connectDB = async () => {
    try {
       await mongoose.connect(process.env.MONGO_URI);
       console.log(`databse connected successfully`);
+        scrapeAndSave()
    } catch (error) {
      console.log(error);
    }
