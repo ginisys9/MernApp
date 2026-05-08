699922547cceb3de9ae72687b48d57743aef2534
Md Intekhab <ginisys9@gmail.com>
2026-05-08 14:39:54 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
-
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <App />
